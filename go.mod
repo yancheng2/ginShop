046a90ecc214586593e95397af410867d615e40a
wangxingkai <yanchengkaige@163.com>
2020-08-21 11:51:49 +0800

    初始化上传

---
+++ go.mod
@@ -0,0 +1,11 @@
+module ginShop
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.16
+	github.com/prometheus/common v0.13.0
+	golang.org/x/net v0.0.0-20200813134508-3edf25e44fcc // indirect
+	gopkg.in/ini.v1 v1.60.0
+)
---
+++ pkg/util/response.go
@@ -0,0 +1,23 @@
+package util
+
+import (
+	"ginShop/pkg/e"
+	"github.com/gin-gonic/gin"
+)
+
+/*
+   数据返回信息的model，格式如下
+*/
+type Response struct {
+	Code int            //自定义编码
+	Msg string          //自定义消息
+	Data interface{}    //返回的数据
+}
+
+func ResponseWithJson(code int, data interface{}, c *gin.Context) {
+	c.JSON(200,&Response{
+		Code:code,
+		Msg:e.GetMsg(code),
+		Data:data,
+	})
+}
